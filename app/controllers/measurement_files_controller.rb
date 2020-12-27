f529db7ef8bc896ee63c783447b30967680250e8
sewerus <seweryn606@gmail.com>
2020-12-27 22:54:49 +0100

    measurement_files, check_access

--- app/controllers/measurement_files_controller.rb
+++ app/controllers/measurement_files_controller.rb
@@ -1,5 +1,7 @@
 class MeasurementFilesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_measurement_file, only: [:destroy]
+  before_action :check_access, only: [:destroy]
 
   def upload_for_measurement
     @file = MeasurementFile.new(measurement_file: params[:file])
@@ -21,6 +23,20 @@ class MeasurementFilesController < ApplicationController
     @measurement_file = MeasurementFile.find_by(id: params[:id])
     if @measurement_file.nil?
       redirect_to root_path, alert: "Nie znaleziono szukanego pliku!"
+    end
+  end
+
+
+  def check_access
+    result = false
+    if current_user.is_a? Admin
+      result = true
+    elsif !@measurement_file.nil?
+      patient = @measurement_file.measurement.patient
+      result = patient.id == current_user.id or patient.dietician.id == current_user.id
+    end
+    unless result
+      redirect_to root_path, alert: "Brak dostępu!"
     end
   end
 end
